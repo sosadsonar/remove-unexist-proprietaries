590eb9846a337fb7e677af2e735e1aab62daf9f0
sosadsonarx <hoangnhat15305@gmail.com>
2023-10-03 21:54:26 +0700

    Refactor code

--- remove-unexist-proprietaries.py
+++ remove-unexist-proprietaries.py
@@ -4,44 +4,54 @@ import argparse
 import re
 
 
-lines = []
-ln = []
-
-
-def create_arg_parser():
-    # Creates and returns the ArgumentParser object
-
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("logDir", help="Path to the log directory.")
     parser.add_argument("proprietariesDir", help="Path to the proprietaries directory.")
-    return parser
+    return parser.parse_args(sys.argv[1:])
 
 
-arg_parser = create_arg_parser()
-parsed_args = arg_parser.parse_args(sys.argv[1:])
-f1 = parsed_args.logDir
-f2 = parsed_args.proprietariesDir
+def find_unknown_sources(log_file):
+    with open(log_file, "r") as f:
+        unknownsources = re.findall(r"(?<=!!\s).+?(?=:\sfile not found in source)", f.read())
+    return unknownsources
 
 
-if os.path.exists(f1):
-    print("Log file found, try to read it!")
-    with open(f1, "r") as file:
-        unknownsources = (re.findall(r"(?<=!!\s).+?(?=:\sfile not found in source)", file.read()))
+def delete_unknown_sources(proprietaries_file, unknown_sources):
+    with open(proprietaries_file, "r") as f:
+        proprietaries_lines = f.readlines()
+
+    deleted_lines = []
+    for num, line in enumerate(proprietaries_lines):
+        for unknown_source in unknown_sources:
+            if unknown_source in line:
+                deleted_lines.append(num)
+                print(f"Deleted: {unknown_source}")
+
+    with open(proprietaries_file, "w") as f:
+        for num, line in enumerate(proprietaries_lines):
+            if num not in deleted_lines:
+                f.write(line)
 
 
-with open(f2, "r") as file:
-    ln = file.readlines()
+def main():
+    args = parse_args()
+
+    log_file = args.logDir
+    proprietaries_file = args.proprietariesDir
+
+    if not os.path.exists(log_file):
+        print("Log file does not exist: {}".format(log_file))
+        sys.exit(1)
+
+    if not os.path.exists(proprietaries_file):
+        print("Proprietaries file does not exist: {}".format(proprietaries_file))
+        sys.exit(1)
+
+    unknown_sources = find_unknown_sources(log_file)
+    delete_unknown_sources(proprietaries_file, unknown_sources)
 
 
-with open(f2, "r") as file:
-    for num, line in enumerate(file):
-        for unknownsource in unknownsources:
-            if unknownsource in line:
-                lines.append(num)
-                print(f"deleted: {unknownsource}")
+if __name__ == "__main__":
+    main()
 
-
-with open(f2, "w") as file:
-    for num, line in enumerate(ln):
-        if num not in lines:
-            file.write(line)
